105f0eedbfc9475ba88415c39974a3031b22078c
pratikbalar <pratik@improwised.com>
2021-09-11 21:23:52 +0530

    fix: replace non working plugin with other

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ for plugin in ${INPUT_THESE}; do
   name="$(cut -d' ' -f1 <<<$plugin)"
   url="$(cut -d' ' -f3 <<<$plugin)"
   version="$(cut -d' ' -f2 <<<$plugin)"
-  echo ${plugin}
   asdf plugin-add ${name} ${url}
   asdf install ${name} ${version}
   asdf global ${name} ${version}
